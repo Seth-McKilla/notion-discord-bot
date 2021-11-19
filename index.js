0a37b5feeec5f4912872668be48c3e613feb7f33
Seth McCullough <seth@endevrs.dev>
2021-11-19 15:30:55 -0500

    Setup basic login script

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
+require("dotenv").config();
+const { Client, Intents } = require("discord.js");
 
-const commands = [
-  {
-    name: "ping",
-    description: "Replies with Pong!",
-  },
-];
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const rest = new REST({ version: "9" }).setToken("token");
+client.once("ready", () => {
+  console.log("Ready!");
+});
 
-(async () => {
-  try {
-    console.log("Started refreshing application (/) commands.");
-
-    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
-      body: commands,
-    });
-
-    console.log("Successfully reloaded application (/) commands.");
-  } catch (error) {
-    console.error(error);
-  }
-})();
+client.login(process.env.DISCORD_TOKEN);
